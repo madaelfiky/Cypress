f349b6486cfc11f4cb20ff1823450578a883bec2
madaelfiky <madaelfiky@gmail.com>
2024-05-07 19:09:35 +0300

    first commit

---
+++ cypress/e2e/workMotionHrTest/hrLoginAddEmp.cy.ts
@@ -0,0 +1,39 @@
+describe("Hr Add Employee",()=> {
+
+  beforeEach("Navigate and Login",() => {
+      cy.visit("https://beta.workmotion.com/login/")
+      
+      cy.get("#email").type("aliaa+qahrmanager@workmotion.com");
+      cy.get("#password").type("Test1234{enter}");
+      cy.viewport(1400,1000)
+
+    })
+
+  it("Hr Add Employee",()=> {
+
+      cy.get("button[data-testid='add-employee-menu']").click();
+      
+      cy.get("li[data-testid='create-new-item']").click();
+
+      cy.wait(20000);
+
+      cy.get("div[class=' css-1wy0on6']").click();
+
+      cy.get("div[class=' css-hlgwow']").type("Germany");
+
+      cy.get("div[class=' css-1joffuw-option']").click();
+
+      cy.get("#onboarding-get-started-btn").click();
+
+      cy.wait(4000);
+      cy.get("div[data-testid='onboarding-product-workglobal'] > div > h3").click();
+
+      cy.get("button[data-cy='product-selection-begin-onboarding-btn']").click();
+
+      cy.get("header[class='sc-dhKdcB jgTtBH']").contains("Talent Def")
+
+
+
+  })
+
+})
