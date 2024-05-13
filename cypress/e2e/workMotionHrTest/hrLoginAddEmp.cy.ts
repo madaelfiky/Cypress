cc5e772e40c5ce264b9ee896630f37b6011a16ff
madaelfiky <madaelfiky@gmail.com>
2024-05-13 13:00:41 +0300

    read from json

--- cypress/e2e/workMotionHrTest/hrLoginAddEmp.cy.ts
+++ cypress/e2e/workMotionHrTest/hrLoginAddEmp.cy.ts
@@ -1,15 +1,17 @@
 describe("Hr Add Employee",()=> {
+  let testData;
 
   beforeEach("Navigate and Login",() => {
-      cy.visit("https://beta.workmotion.com/login/")
-      
-      cy.get("#email").type("aliaa+qahrmanager@workmotion.com");
-      cy.get("#password").type("Test1234{enter}");
-      cy.viewport(1400,1000)      
+    cy.fixture('hrLoginAddEmp.json').then((data) => {
+      testData = data;
+      cy.visit(testData.url); 
+      cy.get("#email").type(testData.credentials.username);
+      cy.get("#password").type(testData.credentials.password+"{enter}");
+      cy.viewport(1400,1000)   
+    });   
     })
 
   it("Hr Add Employee",()=> {
-
       cy.get("button[data-testid='add-employee-menu']").click();
       
       cy.get("li[data-testid='create-new-item']").click();
@@ -20,22 +22,17 @@ describe("Hr Add Employee",()=> {
 
       //cy.get("div[class=' css-hlgwow']").type("Germany");
 
-      cy.xpath("//input[@id='select-country']").type("Germany");
-      //cy.get("input[id='select-country']").type("Germany");
+      cy.xpath("//input[@id='select-country']").type(testData.country);;
 
       cy.get("div[class=' css-1joffuw-option']").click();
 
       cy.get("#onboarding-get-started-btn").click();
 
-      cy.get("div[data-testid='onboarding-product-workglobal'] > div > h3").click();
+      cy.get("div[data-testid='onboarding-product-workglobal']").click();
 
       cy.get("button[data-cy='product-selection-begin-onboarding-btn']").click();
 
-      cy.get("header[class='sc-dhKdcB jgTtBH']").contains("Talent De")
-
-
-
-
+      cy.get("header[class='sc-dhKdcB jgTtBH']").contains(testData.assertionData);
+      
   })
-
 })
